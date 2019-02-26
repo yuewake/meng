d183d2752bca69f8fb603971a3ec42d238c5cd0e
Alan Fang <yuewake@hotmail.com>
2019-02-26 09:50:33 +0800

    项目创建

---
+++ anjia/src/main/java/com/meng/anjia/AnjiaApplication.java
@@ -0,0 +1,13 @@
+package com.meng.anjia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnjiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnjiaApplication.class, args);
+	}
+
+}
