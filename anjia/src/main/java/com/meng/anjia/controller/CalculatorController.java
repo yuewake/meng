ae0de5dd96768448a00cebdfc255953f1e53d352
shine10076 <1007623893@qq.com>
2019-03-08 17:15:34 +0800

    增加了房贷计算器，税费计算器页面和逻辑

---
+++ anjia/src/main/java/com/meng/anjia/controller/CalculatorController.java
@@ -0,0 +1,14 @@
+package com.meng.anjia.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CalculatorController {
+	
+	@RequestMapping("/calculator")
+	public String showCalculatorPath()
+	{
+		return  "calculator_tax";
+	}
+}
