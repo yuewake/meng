2058b18f67463ccbe2e0686600c28a2952d319e4
Alan Fang <yuewake@hotmail.com>
2019-03-09 19:44:29 +0800

    小修改

--- anjia/src/main/java/com/meng/anjia/AnjiaApplication.java
+++ anjia/src/main/java/com/meng/anjia/AnjiaApplication.java
@@ -2,6 +2,8 @@ package com.meng.anjia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class AnjiaApplication {
@@ -9,5 +11,6 @@ public class AnjiaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AnjiaApplication.class, args);
 	}
+
 
 }
